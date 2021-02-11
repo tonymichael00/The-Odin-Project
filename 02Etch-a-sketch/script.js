8161b5a8c44e72ce54b16d1817962b06246ed67a
tonymichael00 <tonymichael00@gmail.com>
2021-02-11 14:56:25 -0600

    final version

--- 02Etch-a-sketch/script.js
+++ 02Etch-a-sketch/script.js
@@ -1,101 +1,111 @@
+
+//sets initial row and height value as well as loads pixels.
 let pixelNo = 3;
-createPixels();
+document.onload = createPixels();
+//let eventArr = [];
+
 sketchTbl = document.getElementById("sketchTbl");
-//console.log(Math.random());
+
+function colorPixels(e) {
+    console.log("target background" + e.target.style.backgroundColor);
+
+    //checkIfRan = false;
+
+    //eventArr.push(e);
+    //console.table(eventArr);
+
+
+    if(e.target && e.target.nodeName == "TD") {
+        //console.log("background" + e.target.style.backgroundColor);
+
+        switch (e.target.style.backgroundColor){
+            case 'rgb(0, 0, 0)':
+                console.log("rgb black ");
+                break;
+
+            case 'rgb(255, 255, 255)':
+                let r = Math.round(Math.random()*255);
+                let g = Math.round(Math.random()*255);
+                let b = Math.round(Math.random()*255);
+  
+                e.target.style.backgroundColor = "rgb(" + r + ", " + g + ", "+ b +")";
+
+                console.log("NEW COLOR rgb: " + r + " ," + g + " ," + b);
+
+                break;
+            
+            default:
+                let rgb = e.target.style.backgroundColor;
+                //console.log("rgb " + rgb);
+
+                let rgbArr = rgb.replace(/[^\d,]/g, '').split(',');
+                
+                //console.table(rgbArr);
+
+                for(let i = 0; i < rgbArr.length; i++){                        
+                    rgbArr[i] = rgbArr[i] - 25.6;
+                    if(rgbArr[i] < 0){
+                        rgbArr[i] = 0;
+                    }
+                }
+
+                e.target.style.backgroundColor = 'rgb(' + rgbArr[0] + ', ' + rgbArr[1] + ', '+ rgbArr[2] + ')';
+                
+                //console.log("rgbArr(" + rgbArr[0] + ", " + rgbArr[1] + ", "+ rgbArr[2] + ")");
+
+                //rgbArr = [];    
+                //this.outerHTML = this.outerHTML;   
+        }
+    };
+}
 
 function createPixels(){
-    let pixelLightness =[];
-    
+    //let checkIfRan;
+
+
+    //creates pixels
 
     for(let i=0; i < pixelNo; i++){
         var tr = document.createElement('tr');
-        // pixelLightness.push([0,0]);
         sketchTbl.appendChild(tr);
-        pixelLightness[i] = [];
-
 
         for(let j=0; j < pixelNo; j++){
-            var td = document.createElement('td');
-            td.style.backgroundColor = "hsl(0,0,100)";
-            sketchTbl.getElementsByTagName("tr")[i].appendChild(td);
-
-            pixelLightness[i][j] = 100;
-            //pixelLightness.splice()               
-
-            //working on this vv
-            // sketchTbl.getElementsByTagName("tr")[i].getElementsByTagName("td")[j].addEventListener("mouseover", function( event ) {
-            //     let h = Math.round(Math.random()*360);
-            //     let s = Math.round(Math.random()*100);
-            //     let l = pixelLightness[i][j];
-            //     // if (pixelLightness[i][j] > 0){
-            //     //     pixelLightness[i][j] -= 10;
-            //     // }
-            //     // console.log(h);
-            //     // console.log(s);
-            //     // console.log(l);
-            //     // console.log(pixelLightness[i][j]);
-            //     pixelLightness[i][j] -= 10;
-
-            //     event.target.style.backgroundColor = "hsl(" + h + ", " + s + "%, " + l + "%)";
-        //};
-            //ADD PARENTHESIS ^^^^^ });
+            var td = document.createElement('TD');
+            sketchTbl.getElementsByTagName("TR")[i].appendChild(td);
+            sketchTbl.getElementsByTagName("TR")[i].getElementsByTagName("TD")[j].style.backgroundColor = 'rgb(255, 255, 255)';
         }
     }
-    console.table(pixelLightness);
-
-    document.getElementById("sketchTbl").addEventListener("mouseover",function(e) {
-        if(e.target && e.target.nodeName == "TD") {
-            let h = Math.round(Math.random()*360);
-            let s = Math.round(Math.random()*100);
-            let l = 50;
-
-            e.target.style.backgroundColor = "hsl(" + h + ", " + s + "%, " + l  + "%)";
-
-            
-            
-            // for(let i=0; i < pixelNo; i++){
-            //     for(let j=0; j < pixelNo; j++){
-            //         pixelLightness[i][j] -= 10;
-            //         l = pixelLightness[i][j];
-            //     }
-            // }
-
-
-            // if (pixelLightness[i][j] > 0){
-            //     pixelLightness[i][j] -= 10;
-            // }
-            // console.log(h);
-            // console.log(s);
-            // console.log(l);
-            // console.log(pixelLightness[i][j]);
-            
-
-
-            
-        };
-
-        //console.table(pixelLightness);
-        
-        console.log("works");
-    });
-
+    //when mouse hovers checks if black, white or colored. If colored reduces RGB values by 10%. If white, assigns random color, if black ignores
+    //if (checkIfRan)
+    document.getElementById("sketchTbl").addEventListener("mouseover", colorPixels);    
     
 
 }
 
+//pixels button
+//THIS IS THE PROBLEM. CREATEPIXELS() RUNS MULTIPLE TIMES
+
 document.getElementById("pixelBtn").addEventListener("click", function( event ) {
     pixelNo = prompt("Enter number of pixels in rows and columns with a max of 100.", "");
+
+    //prompt to enter pixels
 
     while(!(pixelNo <= 100 && pixelNo >= 1)){
         pixelNo = prompt("Important, enter number of pixels in rows and columns with a max of 100.", "");
     }
     
+    //removes the table nodes
+
     function removeAllChildNodes(parent) {
         while (parent.firstChild) {
             parent.removeChild(parent.firstChild);
         }
     }
-
     removeAllChildNodes(sketchTbl);
+
+    //creates new "pixels"/table cells
+    //document.getElementById("sketchTbl").outerHTML = document.getElementById("sketchTbl").outerHTML;
+
+    //document.getElementById("sketchTbl").removeEventListener("mouseover",function(){});
     createPixels();
 });
