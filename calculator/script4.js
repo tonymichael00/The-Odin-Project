52a21ac973fee9a5e405bde755a9d5e717911001
tonymichael00 <tonymichael00@gmail.com>
2021-05-03 17:14:31 -0500

    error squashing, arithmetic works, quick math works, clear works, display works

--- calculator/script4.js
+++ calculator/script4.js
@@ -1,13 +1,11 @@
 //TODO:
-// pressing plus/minus after math operations equals NaN
-// displays can't divide by 0 when entry is 0 even though solution is more than 0
-//quickMath entry is not iterable
+//
 
 //console.log("works");
 let progStart = new Boolean(true);
 let decCheck = new Boolean(true);
 
-let entry = [""];
+let entry = "";
 let solutions = [];
 let operations = [];
 
@@ -195,11 +193,10 @@ function mathCalc(calcKey) {
       numCalc(calcKey);
       // entry += calcKey;
    } else if (
-      (calcKey === "%" ||
-         calcKey === "1/X" ||
-         calcKey === "X^2" ||
-         calcKey === "X^1/2") &&
-      !(entry[entry.length - 1] === "")
+      calcKey === "%" ||
+      calcKey === "1/X" ||
+      calcKey === "X^2" ||
+      calcKey === "X^1/2"
    ) {
       quickCalc(calcKey);
       console.log("quickMath");
@@ -226,25 +223,20 @@ function mathCalc(calcKey) {
    }
 }
 
-//sorts into 4 arithmetic functions. First time sends entry to solutions array and saves operation in array to be handled later.
 function arithCalc(calcKey) {
-   if (entry[entry.length - 1] == "" && progStart === false) {
-      return;
-   }
-   decCheck = true;
    switch (calcKey) {
       case "+":
          if (progStart) {
-            operations.push(addition);
-            solutions.push(parseFloat(entry[entry.length - 1]));
-
-            entry.push("");
+            operations.push(calcKey);
+            solutions.push(parseFloat(entry));
+            entry = "";
             progStart = false;
          } else {
-            operations.push(addition);
-            updateDisplay(operations[operations.length - 2]());
-            solutions.push(operations[operations.length - 2]());
-            entry.push("");
+            operations.push(calcKey);
+            entry =
+               parseFloat(entry) + parseFloat(solutions[solutions.length - 1]);
+            solutions.push(entry);
+            entry = "";
          }
          console.log("solutions.length: " + solutions[solutions.length - 1]);
          console.log("operations");
@@ -254,16 +246,16 @@ function arithCalc(calcKey) {
          break;
       case "-":
          if (progStart) {
-            operations.push(subtraction);
-            solutions.push(parseFloat(entry[entry.length - 1]));
-
-            entry.push("");
+            operations.push(calcKey);
+            solutions.push(parseFloat(entry));
+            entry = "";
             progStart = false;
          } else {
-            operations.push(subtraction);
-            updateDisplay(operations[operations.length - 2]());
-            solutions.push(operations[operations.length - 2]());
-            entry.push("");
+            operations.push(calcKey);
+            entry =
+               parseFloat(solutions[solutions.length - 1]) - parseFloat(entry);
+            solutions.push(entry);
+            entry = "";
          }
          console.log("solutions.length: " + solutions[solutions.length - 1]);
          console.log("operations");
@@ -271,28 +263,18 @@ function arithCalc(calcKey) {
          console.log("solutions");
          console.table(solutions);
          break;
-      case "/":
-         //divide by 0 check
+      case "+":
          if (progStart) {
-            if (parseFloat(entry[entry.length - 1]) === 0) {
-               window.alert("Not even a Jedi can divide by zero.");
-               break;
-            }
-            operations.push(division);
-            solutions.push(parseFloat(entry[entry.length - 1]));
-
-            entry.push("");
+            operations.push(calcKey);
+            solutions.push(parseFloat(entry));
+            entry = "";
             progStart = false;
          } else {
-            if (parseFloat(solutions[solutions.length - 1]) === 0) {
-               window.alert("Not even a Jedi can divide by zero.");
-               break;
-            }
-
-            operations.push(division);
-            updateDisplay(operations[operations.length - 2]());
-            solutions.push(operations[operations.length - 2]());
-            entry.push("");
+            operations.push(calcKey);
+            entry =
+               parseFloat(entry) + parseFloat(solutions[solutions.length - 1]);
+            solutions.push(entry);
+            entry = "";
          }
          console.log("solutions.length: " + solutions[solutions.length - 1]);
          console.log("operations");
@@ -300,18 +282,18 @@ function arithCalc(calcKey) {
          console.log("solutions");
          console.table(solutions);
          break;
-      case "*":
+      case "+":
          if (progStart) {
-            operations.push(multiplication);
-            solutions.push(parseFloat(entry[entry.length - 1]));
-
-            entry.push("");
+            operations.push(calcKey);
+            solutions.push(parseFloat(entry));
+            entry = "";
             progStart = false;
          } else {
-            operations.push(multiplication);
-            updateDisplay(operations[operations.length - 2]());
-            solutions.push(operations[operations.length - 2]());
-            entry.push("");
+            operations.push(calcKey);
+            entry =
+               parseFloat(entry) + parseFloat(solutions[solutions.length - 1]);
+            solutions.push(entry);
+            entry = "";
          }
          console.log("solutions.length: " + solutions[solutions.length - 1]);
          console.log("operations");
@@ -319,32 +301,16 @@ function arithCalc(calcKey) {
          console.log("solutions");
          console.table(solutions);
          break;
+
+         console.log("arithCalc");
    }
 }
 
 function equalCalc(calcKey) {
-   // if (!progStart) {
-   //    operations.push(addition);
-   //    solutions.push(operations[operations.length - 2]());
-   //    entry.push("");
-   // }
-   // console.log("solutions.length: " + solutions[solutions.length - 1]);
-   // console.log("operations");
-   // console.table(operations);
-   // console.log("solutions");
-   // console.table(solutions);
-
    console.log("equalCalc");
 }
 
 function clearCalc(calcKey) {
-   progStart = new Boolean(true);
-   decCheck = new Boolean(true);
-
-   entry = [""];
-   solutions = [];
-   operations = [];
-
    console.log("clearCalc");
 }
 
@@ -359,121 +325,60 @@ function backCalc(calcKey) {
 function quickCalc(calcKey) {
    switch (calcKey) {
       case "%":
-         entry[entry.length - 1] = parseFloat(entry[entry.length - 1]) * 100;
+         entry = parseFloat(entry) * 100;
          break;
       case "1/X":
-         entry[entry.length - 1] = 1 / parseFloat(entry[entry.length - 1]);
+         entry = 1 / parseFloat(entry);
          break;
       case "X^2":
-         entry[entry.length - 1] =
-            entry[entry.length - 1] * entry[entry.length - 1];
+         entry = entry * entry;
          break;
       case "X^1/2":
-         entry[entry.length - 1] = Math.sqrt(entry[entry.length - 1]);
+         entry = Math.sqrt(entry);
          break;
    }
    console.log("quickCalc");
-   console.table(entry);
+   console.log(entry);
 }
 
 function numCalc(calcKey) {
    switch (calcKey) {
       case ".":
-         for (let i = 0; i < entry[entry.length - 1].length; i++) {
-            if (entry[entry.length - 1][i] === ".") {
-               decCheck = false;
-            }
-         }
          if (decCheck) {
-            // entry += calcKey;
-            entry[entry.length - 1] += calcKey;
+            entry += calcKey;
+            decCheck = false;
          }
-         updateDisplay(entry[entry.length - 1]);
          break;
       case "+/-":
-         if (entry[entry.length - 1] === "") {
-            entry[entry.length - 1] = "-";
-            updateDisplay(entry[entry.length - 1]);
-            break;
-         } else if (entry[entry.length - 1] === "-") {
-            entry[entry.length - 1] = "";
-            updateDisplay(entry[entry.length - 1]);
-            break;
-         }
-         entry[entry.length - 1] = entry * -1;
-         entry[entry.length - 1] = entry[entry.length - 1].toString();
-         updateDisplay(entry[entry.length - 1]);
-
+         entry = entry * -1;
+         entry = String(entry);
          break;
       default:
-         let i = 0;
-         let entryCheck = new Boolean(true);
-         if (entry[entry.length - 1][0] === "-" && calcKey === "0") {
-            window.alert("0 can't be negative.");
-            break;
-         }
-         for (let char of entry[entry.length - 1]) {
-            if (char <= 9 && char >= 0) {
-               i++;
-               if (i >= 12) {
-                  window.alert(
-                     "I'm sorry Dave. I can't let you type more than 12 digits."
-                  );
-                  entryCheck = false;
-               }
-            }
-         }
-         if (!entryCheck) {
-            break;
-         }
-         if (entry[entry.length - 1][0] === "0") {
-            entry[entry.length - 1] = "";
-         }
-         entry[entry.length - 1] += calcKey;
-         updateDisplay(entry[entry.length - 1]);
+         entry += calcKey;
          break;
    }
 
    console.log("numCalc");
    console.log("entry");
-   console.table(entry);
+   console.log(entry);
 }
 
-function subtraction() {
-   return (
-      parseFloat(solutions[solutions.length - 1]) -
-      parseFloat(entry[entry.length - 1])
-   );
+function subtraction(num1, num2) {
+   return num1 - num2;
 }
 
-function addition() {
-   return (
-      parseFloat(solutions[solutions.length - 1]) +
-      parseFloat(entry[entry.length - 1])
-   );
+function addition(num1, num2) {
+   return num1 + num2;
 }
 
-function multiplication() {
-   return (
-      parseFloat(solutions[solutions.length - 1]) *
-      parseFloat(entry[entry.length - 1])
-   );
+function multiplication(num1, num2) {
+   return num1 * num2;
 }
 
-function division() {
-   return (
-      parseFloat(solutions[solutions.length - 1]) /
-      parseFloat(entry[entry.length - 1])
-   );
+function division(num1, num2) {
+   return num1 / num2;
 }
 
-function updateDisplay(text) {
-   text = text.toString();
-   if (text.length >= 12) {
-      text = Math.round(parseFloat(text) * 100) / 100;
-      text = Number.parseFloat(text).toExponential(8);
-   }
-   console.log("text.length " + text.length);
-   console.log("text " + text);
-   calcDisplay.textContent = text;
-}
+// function updateDisplay(text){
+
+// }
