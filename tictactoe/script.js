af1ee150703d16dc4ec16e71bcd9347ab66224dd
tonymichael00 <tonymichael00@gmail.com>
2021-07-14 16:18:10 -0500

    did tictactoe project on factories.

---
+++ tictactoe/script.js
@@ -0,0 +1,27 @@
+//TODO
+//
+//NOTES
+//trying out factories. Can switch createO or createX
+
+document.getElementById("playArea").addEventListener("mousedown", function (e) {
+   const newX = createXO(e);
+   newX.createO();
+});
+
+const createXO = (e) => {
+   const createX = () => {
+      // let X = document.createTextNode("X");
+      // let selectDiv = document.querySelector(divClass);
+      // selectDiv = X;
+      e.target.innerText = "X";
+      e.target.style.fontSize = "60px";
+   };
+   const createO = () => {
+      // let X = document.createTextNode("X");
+      // let selectDiv = document.querySelector(divClass);
+      // selectDiv = X;
+      e.target.innerText = "O";
+      e.target.style.fontSize = "60px";
+   };
+   return { createX, createO };
+};
